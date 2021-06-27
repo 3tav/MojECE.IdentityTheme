41e4dc186f436eb5969d7331c90a6bfae8eb9504
Marko Dvornik <marko.dvornik@gmail.com>
2021-06-27 21:40:32 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import myimg from "./myimg.png";
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={myimg} alt="test_image" />
+        <p style={{ "fontFamily": '"Work Sans"' }}>hello world</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
